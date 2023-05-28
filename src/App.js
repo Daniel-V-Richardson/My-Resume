da92aeeff8906bbda4d70ae39908286a64c52de8
Daniel-V-Richardson <richardson20june@gmail.com>
2023-05-28 20:31:47 +0530

    Updated Resume

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <iframe
+        src="/resume.pdf"
+        width="100%"
+        height="600"
+        title="Resume"
+      >
+      </iframe>
+    </div>
+  );
+}
+
+export default App;
